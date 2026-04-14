019755128aa906bbe62272124a084a0cfdf43a15
Wugweb <vedanshu@wugweb.com>
2026-04-14 22:44:07 +0530

    feat: integrate vercel blob and update next.js server external packages and webpack configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverComponentsExternalPackages: ["pdf-parse"],
-      },    
+  serverExternalPackages: ['pdf-parse'],
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
 };
 
 export default nextConfig;
